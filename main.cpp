86328eb917010afb387e4a98b231ca72c993fee1
ZeusDes <mohamed.ahmed.ma19.ma@gmail.com>
2023-02-17 14:52:53 +0200

    Adding main function

--- main.cpp
+++ main.cpp
@@ -3,8 +3,10 @@
 int main()
 {
     std::vector<int> numbers(10);
-    for(int i = 0; i < 10; i++) {
+    for(int i = 0; i < 10; i++)
+    {
         numbers[i] = i + 1;
     }
     std::cout << "Sum: " << getSum(numbers) << std::endl;
+    std::cout << "Min: " << getMin(numbers) << std::endl;
 }
